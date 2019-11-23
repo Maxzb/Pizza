f8447fba75c5024bc95da7c4a97b300220a17bfe
maxim_bredikhin <maxzb86@gmail.com>
2019-11-24 01:17:26 +0300

    Header с каруселью

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+   $("#carousel1").owlCarousel({
+      items:1,
+      margin:160,
+      loop:true,
+      nav:true,
+      navText: ["<i class='fas fa-chevron-left'>", "<i class='fas fa-chevron-right'>"]
+   });
+ });
