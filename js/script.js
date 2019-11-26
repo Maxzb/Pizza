9453df21db8d56648bfc88a8e1b5cdd3cd8826f1
maxim_bredikhin <maxzb86@gmail.com>
2019-11-26 22:20:50 +0300

    DEL and PAY

--- js/script.js
+++ js/script.js
@@ -8,8 +8,6 @@ $(document).ready(function(){
       navClass: ["owl-prev", "owl-next"],
       navSpeed: 500
    });
- });
- $(document).ready(function(){
    $("#carousel2").owlCarousel({
       items:3,
       margin:10,
@@ -17,7 +15,37 @@ $(document).ready(function(){
       nav:true,
       navText: ["<div class='pizza-block-nav pizza-block-nav-left'><i class='fas fa-chevron-left pizza-block-nav__arrow-left'></i></div>", "<div class='pizza-block-nav pizza-block-nav-right'><i class='fas fa-chevron-right pizza-block-nav__arrow-right'></div>"],
       navClass: ["pizza-block-nav__arrow-left", "pizza-block-nav__arrow-right"],
-      navSpeed: 500/* ,
-      dots: true */
+      navSpeed: 500
    });
+   $("#carousel3").owlCarousel({
+      items:4,
+      margin:25,
+      nav:true,
+      navText: ["<i class='fas fa-chevron-left'>", "<i class='fas fa-chevron-right'>"],
+      navClass: ["owl-prev", "owl-next"],
+      navSpeed: 300
+   });
+   $("#carousel4").owlCarousel({
+      items:3,
+      margin:50,
+      loop:true,
+      nav:true,
+      navText: ["<i class='fas fa-chevron-left'>", "<i class='fas fa-chevron-right'>"],
+      navClass: ["owl-prev", "owl-next"],
+      navSpeed: 300,
+      center: true
+   });
+
+   /* Маска для телефона */
+   $(function(){
+      $("#phone").mask("8(999) 999-9999");
+    });
+
+    /* Стилизация Скроллбара */
+    $(".inner-block__order-list").mCustomScrollbar({
+  });
  });
+
+ 
+
+ 
